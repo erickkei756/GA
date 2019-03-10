ea6e13ff6c5a862e7bea8e66857d7c79834b2c09
erickkei756 <47996659+erickkei756@users.noreply.github.com>
2019-03-09 20:56:32 -0700

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,82 @@
+# homework 1
+
+beeps = 2
+boops = 6
+total_1 = beeps + boops
+print(total_1)
+
+beeps = 0
+boops = 5
+total_2 = beeps + boops
+print(total_2)
+
+beeps = 9
+boops = 3
+total_3 = beeps + boops
+print(total_3)
+
+beeps = 4
+boops = 8
+total_4 = beeps + boops
+print(total_4)
+
+beeps = 10
+boops = 5
+total_5 = beeps + boops
+print(total_5)
+
+BOP = " "
+
+beeps = 11
+boops = 12
+total_6 = beeps + boops
+print(total_6)
+
+beeps = 11
+boops = 4
+total_7 = beeps + boops
+print(total_7)
+
+beeps = 1
+boops = 17
+total_8 = beeps + boops
+print(total_8)
+
+beeps = 5
+boops = 7
+total_9 = beeps + boops
+print(total_9)
+
+beeps = 4
+boops = 0
+total_10 = beeps + boops
+print(total_10)
+
+one = 'A'
+two = 'B'
+three = 'C'
+four = 'D'
+five = 'E'
+six = 'F'
+seven = 'G'
+eight = 'H'
+nine = 'I'
+ten = 'J'
+eleven = 'K'
+twelve = 'L'
+thirteen = 'M'
+fourteen = 'N'
+fifteen = 'O'
+sixteen = 'P'
+seventeen = 'Q'
+eighteen = 'R'
+nineteen = 'S'
+twenty = 'T'
+twentyone = 'U'
+twentytwo = 'V'
+twentythree = 'W'
+twentyfour = 'X'
+twentyfive = 'Y'
+twentysix = 'Z'
+
+print(eight,five,twelve,twelve,fifteen,BOP,twentythree,fifteen,eighteen,twelve,four)
