c4e622af8f1a781324350d120cd39071c21d5bcc
erickkei756 <47996659+erickkei756@users.noreply.github.com>
2019-03-18 19:20:17 -0600

    Add files via upload

---
+++ hw5_problem1.py
@@ -0,0 +1,13 @@
+
+romantic_movie1 = ("The Princess Bride", 1987, 98, "The story of a man and a woman who lived happily ever after.", ["Buttercup", "Westley", "Fezzik", "Inigo Montoya", "Vizzini"])
+romantic_movie2 = ("Groundhog Day", 1993, 101, "He's having the day of his life… over and over again.", ["Phil Connors"])
+romantic_movie3 = ("Amélie", 2001, 122, "One person can change your life forever.", ["Amélie Poulain", "Nino Quincampoix", "The Garden Gnome"])
+
+intro = ("Here are my favorite romance movies: ")
+
+
+print(intro+romantic_movie1[0]+" ("+str(romantic_movie1[1])+"): "+str(romantic_movie1[3]))
+
+print(intro+romantic_movie2[0]+" ("+str(romantic_movie2[1])+"): "+str(romantic_movie2[3]))
+
+print(intro+romantic_movie3[0]+" ("+str(romantic_movie3[1])+"): "+str(romantic_movie3[3]))
